14c3654a195be5d322257339f5fe8564ab928854
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:30:25 +0000

    Chanterelle Card File

---
+++ src/cards/Chanterelle.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class Chanterelle extends Mushroom{
+
+  public Chanterelle(CardType card){
+    super(card, "chanterelle");
+    flavourPoints = 4;
+    sticksPerMushroom = 2;
+  }
+
+}
