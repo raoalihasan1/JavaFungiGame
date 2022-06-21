5f0da76fb078b9c49ac274f1bac4c361f2bf4159
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:28:37 +0000

    BirchBolete Card File

---
+++ src/cards/BirchBolete.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class BirchBolete extends Mushroom{
+
+  public BirchBolete(CardType card){
+    super(card, "birchbolete");
+    flavourPoints = 3;
+    sticksPerMushroom = 2;
+  }
+
+}
