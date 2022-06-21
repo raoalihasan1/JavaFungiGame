115a34978261017a076ed87f6fe955a374878ac9
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:32:13 +0000

    HoneyFungus Card File

---
+++ src/cards/HoneyFungus.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class HoneyFungus extends Mushroom{
+
+  public HoneyFungus(CardType card){
+    super(card, "honeyfungus");
+    flavourPoints = 1;
+    sticksPerMushroom = 1;
+  }
+
+}
