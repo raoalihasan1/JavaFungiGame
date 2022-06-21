06d0db4fa31a3659aaf305f13870445954ef9e82
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:34:49 +0000

    Shiitake Card File

---
+++ src/cards/Shiitake.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class Shiitake extends Mushroom{
+
+  public Shiitake(CardType card){
+    super(card, "shiitake");
+    flavourPoints = 2;
+    sticksPerMushroom = 2;
+  }
+
+}
