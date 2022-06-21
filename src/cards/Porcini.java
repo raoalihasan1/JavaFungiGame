34210098a4abdcab586ef3f5e7b03349f8e722cb
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:34:25 +0000

    Porcini Card File

---
+++ src/cards/Porcini.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class Porcini extends Mushroom{
+
+  public Porcini(CardType card){
+    super(card, "porcini");
+    flavourPoints = 3;
+    sticksPerMushroom = 3;
+  }
+
+}
