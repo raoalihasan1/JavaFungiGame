474ea3cc94f8a354424a46cd54195e53c34776eb
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:32:58 +0000

    LawyersWig Card File

---
+++ src/cards/LawyersWig.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class LawyersWig extends Mushroom{
+
+  public LawyersWig(CardType card){
+    super(card, "lawyerswig");
+    flavourPoints = 2;
+    sticksPerMushroom = 1;
+  }
+
+}
