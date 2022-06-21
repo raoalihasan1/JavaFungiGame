74489ac7dd3bbf23998e90fac390ba8f5769aa73
Ali Hasan <alirao2880@gmail.com>
2022-06-21 13:35:36 +0000

    TreeEar Card File

---
+++ src/cards/TreeEar.java
@@ -0,0 +1,11 @@
+package cards;
+
+public class TreeEar extends Mushroom{
+
+  public TreeEar(CardType card){
+    super(card, "treeear");
+    flavourPoints = 1;
+    sticksPerMushroom = 2;
+  }
+
+}
